c5317c6e706973314fb0a7ae8b0f18d69be0cb67
opsrc <opsrc7094@gmail.com>
2024-01-18 01:23:37 +0530

    core/apis/assignments/principal.py Now principal can grade or re-grade assignment

---
+++ core/apis/teachers/principal.py
@@ -0,0 +1,36 @@
+from flask import Blueprint
+from core import db
+from core.apis import decorators
+from core.apis.responses import APIResponse
+from core.models.assignments import Assignment
+
+principal_assignments_resources = Blueprint('principal_teachers_resources', __name__)
+
+# Todo : Create a TeacherSchema  
+from .schema import TeacherSchema
+ 
+
+@principal_assignments_resources.route('/teachers', methods=['GET'], strict_slashes=False)
+@decorators.authenticate_principal
+def list_teachers(p):
+    """Returns list of assignments"""
+    teachers = Assignment.filter(Assignment.state == "SUBMITTED")
+    teachers_dump = TeachersSchema().dump(teachers, many=True)
+    return APIResponse.respond(data=teachers_dump)
+
+
+@principal_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
+@decorators.accept_payload
+@decorators.authenticate_principal
+def grade_assignment(p, incoming_payload):
+    """Grade an assignment"""
+    grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
+
+    graded_assignment = Assignment.mark_grade(
+        _id=grade_assignment_payload.id,
+        grade=grade_assignment_payload.grade,
+        auth_principal=p
+    )
+    db.session.commit()
+    graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
+    return APIResponse.respond(data=graded_assignment_dump)
