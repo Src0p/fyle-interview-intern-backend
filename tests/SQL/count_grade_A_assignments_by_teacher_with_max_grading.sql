708aa8c6b9cb5cfd62864fb411582a5fb51d9bd8
Arsh <opsrc7094@gmail.com>
2024-01-19 11:09:12 +0530

    tests/SQL/ Done with SQL tests

--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,1 +1,5 @@
 -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+
+select count(*) from assignments where teacher_id in (select teacher_id from assignments where teacher_id is not null group by teacher_id order by count(*) desc limit 1) and grade = 'A';   
+
+-- select teacher_id, count(*) as assignments_count from assignments where teacher_id is not null group by teacher_id order by assignments_count desc limit 1 ;
--- tests/SQL/number_of_assignments_per_state.sql
+++ tests/SQL/number_of_assignments_per_state.sql
@@ -1,1 +1,2 @@
 -- Write query to get number of assignments for each state
+select state, count(*) as state_count from assignments group by state ; 
